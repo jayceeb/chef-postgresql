b81bf2cc609021fda22b1ada75e635c8089adb4b
Jeff Harvey-Smith <jeff@clearstorydata.com>
2014-04-22 16:43:10 -0700

    Add support for ubuntu 14.04 trusty

--- metadata.rb
+++ metadata.rb
@@ -13,6 +13,7 @@ recipe            "postgresql::server_streaming_slave", "Install s postgresql st
 recipe            "postgresql::server", "Installs postgresql server packages, templates"
 recipe            "postgresql::server_redhat", "Installs postgresql server packages, redhat family style"
 recipe            "postgresql::server_debian", "Installs postgresql server packages, debian family style"
+recipe            "postgresql::wal-e", "Installs wal-e S3 backup process for postgres"
 
 
 supports "ubuntu", "< 14.10"
